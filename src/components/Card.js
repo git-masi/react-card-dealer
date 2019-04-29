cfb00c9fe9cc5b841dad015c0f5f56b4ad1523a4
Eric Masi <eric@gitmasi.com>
2019-04-29 12:20:10 -0400

    set state to display card on button click

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,9 @@ import './Card.css';
 
 const Card = props => {
   return (
-    <div></div>
+    <div>
+      <img style={{height: '100px', width: '100px'}} src={props.image} alt={`${props.value} of ${props.suit}`}/>
+    </div>
   )
 }
 
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,24 +2,53 @@ import React, { Component } from 'react';
 import './Deck.css';
 import Card from './Card';
 import axios from 'axios';
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
   
 class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deck: null
+      deck: null,
+      drawnCards: []
     }
+    this.getCard = this.getCard.bind(this);
   }
 
   async componentDidMount() {
-    let response = await axios.get(API_URL);
-    console.log(response.data.deck_id);
+    let response = await axios.get(`${API_BASE_URL}new/shuffle/`);
+    // console.log(response.data.deck_id);
+    this.setState({deck: response.data});
+  }
+
+  async getCard () {
+    try {
+      let cardRes = await axios.get(`${API_BASE_URL}${this.state.deck.deck_id}/draw/`);
+      // console.log(cardRes.data);
+      if (!cardRes.data.success) {
+        throw new Error('No cards remaining');
+      }
+      let card = cardRes.data.cards[0];
+      this.setState({drawnCards: [...this.state.drawnCards, card]});
+    } catch (err) {
+      alert(err);
+    }
   }
 
   render() {
+    const displayCards = this.state.drawnCards.map(card => (
+      <Card 
+        image={card.image}
+        suit={card.suit}
+        value={card.value}
+        key={card.code}
+      />
+    ));
     return (
-      <div>cards or something here</div>
+      <div>
+        <h1>Deal Me In!</h1>
+        <button onClick={this.getCard}>Deal!</button>
+        <div>{displayCards}</div>
+      </div>
     )
   }
 }
